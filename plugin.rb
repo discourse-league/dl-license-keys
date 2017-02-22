41e070844bc319dc66a42b7ab3a836b592ab184a
Joe Buhlig <joe@joebuhlig.com>
2017-02-22 09:44:05 -0600

    Prevent license key email at registration

--- plugin.rb
+++ plugin.rb
@@ -77,7 +77,7 @@ after_initialize do
       def execute(args)
         group = Group.find(args[:group_id])
 
-        licenses = PluginStore.get("dl_license_keys", "licenses").select{|license| license[:group_id] = group.id} || []
+        licenses = PluginStore.get("dl_license_keys", "licenses").select{|license| license[:group_id] == group.id} || []
         
         if !licenses.blank?
 
